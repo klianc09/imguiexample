f885e1aed2e5b408b2763e87942a406784f5c707
Anton Klinger <klianc09@gmail.com>
2018-04-06 17:17:10 +0200

    LibGDX project generated using Gdx-Setup by czyzby
    https://github.com/czyzby/gdx-setup (1.9.6-beta5)

---
+++ core/src/main/java/at/example/Main.java
@@ -0,0 +1,7 @@
+package at.example;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
