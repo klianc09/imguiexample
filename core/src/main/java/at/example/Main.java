5cebbba97efecc8a312430ac0095edaaaaf93a28
Anton Klinger <klianc09@gmail.com>
2023-03-01 16:52:54 +0100

    generate new project template with gdx-liftoff-1.11.0.7

--- core/src/main/java/at/example/Main.java
+++ core/src/main/java/at/example/Main.java
@@ -2,55 +2,33 @@ package at.example;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Graphics;
 import com.badlogic.gdx.graphics.GL20;
-import imgui.Context;
-import imgui.ImGui;
-import imgui.impl.ImplGL3;
-import imgui.impl.LwjglGlfw;
-import uno.glfw.GlfwWindow;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-/**
- * {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms.
- */
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-
-    //ImGui Singleton Instances
-    private LwjglGlfw lwjglGlfw = LwjglGlfw.INSTANCE;
-    private ImplGL3 lwjglGL3 = ImplGL3.INSTANCE;
-    private ImGui imgui = ImGui.INSTANCE;
-
-    private Context ctx;
+    private SpriteBatch batch;
+    private Texture image;
 
     @Override
     public void create() {
-        ctx = new Context();
-
-        Lwjgl3Graphics lwjgl3Graphics = (Lwjgl3Graphics) Gdx.graphics;
-        long windowHandle = lwjgl3Graphics.getWindow().getWindowHandle();
-        lwjglGlfw.init(new GlfwWindow(windowHandle), true, LwjglGlfw.GlfwClientApi.OpenGL);
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-        lwjglGlfw.newFrame();
-
-        boolean[] showDemo = {true};
-        imgui.showDemoWindow(showDemo);
-
-        imgui.render();
-
-        if (imgui.getDrawData() != null) {
-            lwjglGL3.renderDrawData(imgui.getDrawData());
-        }
+        Gdx.gl.glClearColor(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
     }
 
     @Override
     public void dispose() {
-        lwjglGlfw.shutdown();
-        ctx.shutdown();
+        batch.dispose();
+        image.dispose();
     }
 }
